6d10b823c88c508454774b1f9feca09733848663
JeonSeungGwon <jeon87721@gmail.com>
2022-05-09 23:16:42 +0900

    blockcmp implementation

--- main.c
+++ main.c
@@ -57,7 +57,11 @@ void sizecmp(){
 
 //두 개의 파일 블락 수를 비교하는 함수 작성
 void blockcmp(){
-    
+    printf("\nblock compare");
+    if (stat1.st_blocks > stat2.st_blocks) printf("\nfile1 has more blocks\n");
+    else if (stat1.st_blocks < stat2.st_blocks) printf("\nfile2 has more blocks\n");
+    else printf("\nboth block sizes are same\n");
+
 }
 
 //두 개의 파일 수정 날짜를 비교하는 함수 작성
